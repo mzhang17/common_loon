f0843d3b7c54bac0f37d4b9d3cb7db1428530114
mzhang17 <mzhang17@tulane.edu>
2024-12-02 14:54:45 -0600

    initiation
    Copied and pasted working codes into GitHub repository. Learning how to use GitHub for version control.

---
+++ developmentCode.R
@@ -0,0 +1,30 @@
+
+lowest_b_y <- list(
+  b_a=0.8,   # adult pairing propensity
+  b_y=0.3, # young adult pairing propensity
+  m=0.48, # chick production.    
+  r=0.5,   #sex ratio   
+  sigma_j = 0.45^(12/34), #juv survival
+  Pa=0.92,  # Adult survival
+  sigma_y=0.92 # young adult survival
+)
+
+lower_b_y <- list(
+  b_a=0.8,   # adult pairing propensity
+  b_y=0.5, # young adult pairing propensity
+  m=0.48, # chick production.    
+  r=0.5,   #sex ratio   
+  sigma_j = 0.45^(12/34), #juv survival
+  Pa=0.92,  # Adult survival
+  sigma_y=0.92 # young adult survival
+)
+
+high_b_y <- list(
+  b_a=0.8,   # adult pairing propensity
+  b_y=0.7, # young adult pairing propensity
+  m=0.48, # chick production.    
+  r=0.5,   #sex ratio   
+  sigma_j = 0.45^(12/34), #juv survival
+  Pa=0.92,  # Adult survival
+  sigma_y=0.92 # young adult survival
+)
